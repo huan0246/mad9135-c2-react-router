1dad9fbdff91532c760ac776f73bb456f7e20593
Huang Chunyen <huan0246@algonquinlive.com>
2021-10-10 18:04:15 -0400

    Add array sort by last name in Addresslist.js

--- src/components/Addresslist/Addresslist.js
+++ src/components/Addresslist/Addresslist.js
@@ -12,29 +12,39 @@ export default function Addresslist(props) {
           <thead>
             <tr>
               <th>Name</th>
-              <th>Address</th>  
+              <th>Address</th>
             </tr>
           </thead>
           <tbody>
-          {props.userdata.map((user, index) => (
-            <tr key={index + 1}>
-              <td>
-                <NavLink to={`/userlist/${index + 1}`}>
-                  {user && `${user.name.first} ${user.name.last}`}
-                </NavLink>
-              </td>
-              <td>
-                <p>
-                  {user &&
-                    `${user.location.postcode} ${user.location.street.number} ${user.location.street.name}`}
-                </p>
-                <p>
-                  {user &&
-                    `${user.location.city}, ${user.location.state}, ${user.location.country}`}
-                </p>
-              </td>
-            </tr>
-          ))}
+            {props.userdata
+              .sort((i, j) => {
+                if (i.name.last > j.name.last) {
+                  return 1;
+                } else if (i.name.last < j.name.last) {
+                  return -1;
+                } else {
+                  return 0;
+                }
+              })
+              .map((user, index) => (
+                <tr key={index + 1}>
+                  <td>
+                    <NavLink to={`/userlist/${index + 1}`}>
+                      {user && `${user.name.first} ${user.name.last}`}
+                    </NavLink>
+                  </td>
+                  <td>
+                    <p>
+                      {user &&
+                        `${user.location.postcode} ${user.location.street.number} ${user.location.street.name}`}
+                    </p>
+                    <p>
+                      {user &&
+                        `${user.location.city}, ${user.location.state}, ${user.location.country}`}
+                    </p>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
