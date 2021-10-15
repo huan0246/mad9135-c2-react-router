00ad52623bbdf35eebfd13bcd04512915f288d74
Huang Chunyen <huan0246@algonquinlive.com>
2021-10-14 21:31:57 -0400

    Add content in home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,12 @@ import './home.css'
 export default function Home() {
   return (
     <div className="home">
-      This is home page
+      <h1>- Welcome -</h1>
+      <ul>
+        <li>You can click User List to check user card.</li>
+        <li>Click user card it will show detail information of the user.</li>
+        <li>You can also access user information by clicking Address List.</li>
+      </ul>
     </div>
   )
 }
